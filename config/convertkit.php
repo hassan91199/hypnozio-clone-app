90a447bfcfaf8b11810f22cad206e7cdcaf9ae77
Hassan <hassanabbas4745@gmail.com>
2024-05-26 12:39:13 +0500

    make config for the convertkit load key from env

---
+++ config/convertkit.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | ConvertKit API Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the API key and secret for your ConvertKit account.
+    |
+    */
+
+    'api_key' => env('CONVERTKIT_API_KEY'),
+    'api_secret' => env('CONVERTKIT_API_SECRET'),
+    'quiz_form_id' => env('CONVERTKIT_QUIZ_FORM_ID'),
+];
